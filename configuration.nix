d3003435c985a29003f4460e405c53c81f7f4d87
npc-z <1763998996@qq.com>
2024-01-28 16:12:49 +0800

    format

--- configuration.nix
+++ configuration.nix
@@ -1,18 +1,10 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
-    imports =
-        [ # Include the results of the hardware scan.
+    imports = [ # Include the results of the hardware scan.
         ./hardware-configuration.nix
-        ];
+    ];
 
-# Use the systemd-boot EFI boot loader.
-# boot.loader.systemd-boot.enable = true;
-# boot.loader.efi.canTouchEfiVariables = true;
 
     fonts.packages = with pkgs; [
         (nerdfonts.override {
@@ -21,12 +13,9 @@
             "FiraCode"
             ];
         })
-        noto-fonts
-        noto-fonts-cjk
-        noto-fonts-emoji
+        noto-fonts noto-fonts-cjk noto-fonts-emoji
         liberation_ttf
-        fira-code
-        fira-code-symbols
+        fira-code fira-code-symbols
         mplus-outline-fonts.githubRelease
         dina-font
         proggyfonts
@@ -62,40 +51,11 @@
             '';
     };
 
-# networking.hostName = "nixos"; # Define your hostname.
-# Pick only one of the below networking options.
-# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-# networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-# Set your time zone.
+    # Set your time zone.
     time.timeZone = "Asia/Shanghai";
 
-# Configure network proxy if necessary
-# networking.proxy.default = "http://user:password@proxy:port/";
-# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-# Select internationalisation properties.
-# i18n.defaultLocale = "en_US.UTF-8";
-# console = {
-#   font = "Lat2-Terminus16";
-#   keyMap = "us";
-#   useXkbConfig = true; # use xkb.options in tty.
-# };
-
-# Enable the X11 windowing system.
-# services.xserver.enable = true;
-
-
-
-
-# Configure keymap in X11
-# services.xserver.xkb.layout = "us";
-# services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-# Enable CUPS to print documents.
-# services.printing.enable = true;
-
-# Enable sound.
+    # Enable sound.
     sound.enable = true;
     hardware.pulseaudio.enable = false;
     services.pipewire = {
@@ -106,48 +66,20 @@
 
     security.rtkit.enable = true;
 
-# Enable touchpad support (enabled default in most desktopManager).
-# services.xserver.libinput.enable = true;
     nix.settings = {
         experimental-features = [
             "nix-command"
-                "flakes"
+            "flakes"
         ];
         trusted-users = ["npc"];
         substituters = [
             "https://mirror.sjtu.edu.cn/nix-channels/store"
-                "https://cache.nixos.org"
+            "https://cache.nixos.org"
         ];
     };
 
-# Define a user account. Don't forget to set a password with ‘passwd’.
-# users.users.alice = {
-#   isNormalUser = true;
-#   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-#   packages = with pkgs; [
-#     firefox
-#     tree
-#   ];
-# };
-
-# List packages installed in system profile. To search, run:
-# $ nix search wget
-# environment.systemPackages = with pkgs; [
-#   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-#   wget
-# ];
-
-# Some programs need SUID wrappers, can be configured further or are
-# started in user sessions.
-# programs.mtr.enable = true;
-# programs.gnupg.agent = {
-#   enable = true;
-#   enableSSHSupport = true;
-# };
-
-# List services that you want to enable:
-
-# Enable the OpenSSH daemon.
+    # List services that you want to enable:
+    # Enable the OpenSSH daemon.
     services.openssh.enable = true;
     services.keyd.enable = true;
     services.v2raya.enable = true;
@@ -157,9 +89,6 @@
 
     environment.variables.EDITOR = "vim";
     users.defaultUserShell = pkgs.zsh;
-
-    # environment.shellAliases = {
-    # };
 
     programs = {
         hyprland = {
@@ -205,80 +134,27 @@
     nixpkgs.config.allowUnfree = true;
 
     environment.systemPackages = with pkgs; [
-            python311
-            python311Packages.pip
-            gnumake
-            gcc9
-            zsh
-            go
-            nodejs_20
-            fd
-            ripgrep
-            cmake
-            unzip
-            vscode
-            which
-            tree
-            git
-            lazygit
-            vim
-            neovim
-            curl
-            wget
-# tools
-            glow
-            kitty
-            foot
-            cinnamon.nemo
-            vscode
-            zathura
-            wofi
-            netease-cloud-music-gtk
-#
-            neofetch
-            btop
-            lsof
-            keyd
-            stow
-            v2raya
-# desktop
-            microsoft-edge
-            hyprland
-            waybar
-            swaybg
-            brightnessctl
-            pamixer
-            fcitx5
-            ];
+        python311 python311Packages.pip go nodejs_20
+        gnumake gcc9 cmake
+        lsof fd ripgrep unzip which tree
+        vscode
+        git lazygit
+        vim neovim
+        curl wget
+        # tools
+        kitty foot
+        cinnamon.nemo glow zathura
+        netease-cloud-music-gtk
 
-# Open ports in the firewall.
-# networking.firewall.allowedTCPPorts = [ ... ];
-# networking.firewall.allowedUDPPorts = [ ... ];
-# Or disable the firewall altogether.
-# networking.firewall.enable = false;
+        neofetch btop
+        zsh keyd stow
+        # desktop
+        microsoft-edge
+        hyprland waybar swaybg wofi
+        brightnessctl pamixer
+        fcitx5 v2raya
+    ];
 
-# Copy the NixOS configuration file and link it from the resulting system
-# (/run/current-system/configuration.nix). This is useful in case you
-# accidentally delete configuration.nix.
-# system.copySystemConfiguration = true;
-
-# This option defines the first version of NixOS you have installed on this particular machine,
-# and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-#
-# Most users should NEVER change this value after the initial install, for any reason,
-# even if you've upgraded your system to a new NixOS release.
-#
-# This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-# so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-# to actually do that.
-#
-# This value being lower than the current NixOS release does NOT mean your system is
-# out of date, out of support, or vulnerable.
-#
-# Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-# and migrated your data accordingly.
-#
-# For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
     system.stateVersion = "24.05"; # Did you read the comment?
 }
 
