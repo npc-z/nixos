9bf996ed8163b416bdce3fdfa1cd2033cb4eb552
npc-z <1763998996@qq.com>
2024-02-05 12:18:11 +0800

    模块化配置 waybar

--- home.nix
+++ home.nix
@@ -5,14 +5,11 @@
     home.username = "npc";
     home.homeDirectory = "/home/npc";
 
+    # programs
+    imports = [
+        ./home/programs/waybar
+    ];
     # 递归将某个文件夹中的文件，链接到 Home 目录下的指定位置
-    home.file.".config/waybar/scripts" = {
-      source = ./dotfiles/waybar/.config/waybar/scripts;
-      recursive = true;   # 递归整个文件夹
-      executable = true;  # 将其中所有文件添加「执行」权限
-    };
-    home.file.".config/waybar/config".source = ./dotfiles/waybar/.config/waybar/hyprland/config;
-    home.file.".config/waybar/style.css".source = ./dotfiles/waybar/.config/waybar/hyprland/style.css;
 
     # 直接以 text 的方式，在 nix 配置文件中硬编码文件内容
     # home.file.".xxx".text = ''
