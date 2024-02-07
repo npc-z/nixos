2cc9c2129a51e2f02925c93fc8c3a94351d8ffb0
npc-z <1763998996@qq.com>
2024-02-07 19:54:09 +0800

    update user config

--- systems/user.nix
+++ systems/user.nix
@@ -3,9 +3,13 @@
 {
     users.defaultUserShell = pkgs.zsh;
 
+    # forbid `useradd` to add user
+    # users.mutableUsers = false;
+
     users.users.npc = {
         isNormalUser = true;
         description = "npc";
+        hashedPassword = "$y$j9T$hzAVLzc8eIFShnzWJiye/0$Wo5QHjsi98zBMcUqlUZ8Ehe6JKcekiI/rBcWLgDCuh9";
         extraGroups = [
             "networkmanager"
             "wheel"
