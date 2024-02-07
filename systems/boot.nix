040c8471f980ef06a09c1c5d0cf3912badf23cb0
npc-z <1763998996@qq.com>
2024-02-07 17:26:20 +0800

    add grub theme

--- systems/boot.nix
+++ systems/boot.nix
@@ -21,4 +21,27 @@
             efiSysMountPoint = "/boot";
         };
     };
+
+    boot.loader.grub.theme = pkgs.stdenv.mkDerivation {
+        pname = "distro-grub-themes";
+        version = "3.1";
+        src = pkgs.fetchFromGitHub {
+            owner = "AdisonCavani";
+            repo = "distro-grub-themes";
+            rev = "v3.1";
+            hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+        };
+        installPhase = "cp -r customize/nixos $out";
+    };
+
+    # boot.loader.grub.theme = pkgs.stdenv.mkDerivation {
+    #     name = "bigsur-grub2-theme";
+    #     src = pkgs.fetchFromGitHub {
+    #         owner = "Teraskull";
+    #         repo = "bigsur-grub2-theme";
+    #         rev = "5bf0a9711282e4463eec82bb4430927fdc9c662a";
+    #         hash = "sha256-BSZHTd6Eg/QZ1ekGTd3W+xHI6RbSmwCrcDxaCWD/DbI=";
+    #     };
+    #     installPhase = "cp -r bigsur $out";
+    # };
 }
