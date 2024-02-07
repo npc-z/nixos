a45a791cae79e95df9adcf795f7787287d59773b
npc-z <1763998996@qq.com>
2024-02-07 09:28:47 +0800

    add pkg mitscheme for learning

--- home.nix
+++ home.nix
@@ -30,6 +30,7 @@
     # 这些软件将仅在当前用户下可用，不会影响系统级别的配置
     # 建议将所有 GUI 软件，以及与 OS 关系不大的 CLI 软件，都通过 home.packages 安装
     home.packages = with pkgs;[
+        mitschemeX11
     ];
 
     # alacritty - 一个跨平台终端，带 GPU 加速功能
