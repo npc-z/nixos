67fab82a69eb5b30c66bfa871f89f45fc2dc2e36
npc-z <1763998996@qq.com>
2024-02-09 20:27:38 +0800

    [format code] services.nix

--- systems/services.nix
+++ systems/services.nix
@@ -1,32 +1,30 @@
-{ config, lib, pkgs, ... }:
+{...}: {
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.v2raya.enable = true;
 
-{
-    # Enable the OpenSSH daemon.
-    services.openssh.enable = true;
-    services.v2raya.enable = true;
-
-    services.keyd = {
-        enable = true;
-        keyboards = {
-            default = {
-                ids = ["*"];
-                settings = {
-                    main = {
-                        # capslock = "control"; # do not work
-                        capslock = "oneshot(control)";
-                        "'" = ''"'';
-                    };
-                    control = {
-                        h = "left";
-                        j = "down";
-                        k = "up";
-                        l = "right";
-                    };
-                    shift = {
-                        "'" = "'";
-                    };
-                };
-            };
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        ids = ["*"];
+        settings = {
+          main = {
+            # capslock = "control"; # do not work
+            capslock = "oneshot(control)";
+            "'" = ''"'';
+          };
+          control = {
+            h = "left";
+            j = "down";
+            k = "up";
+            l = "right";
+          };
+          shift = {
+            "'" = "'";
+          };
         };
+      };
     };
+  };
 }
