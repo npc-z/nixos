9be9e836f8a182ddf64d9d367ecc9ac63b1e3264
npc-z <1763998996@qq.com>
2024-03-30 09:08:41 +0800

    add deploy-rs and a mini os installation config

--- flake.nix
+++ flake.nix
@@ -21,6 +21,12 @@
     nur.url = "github:nix-community/NUR";
     # 强制 NUR 和该 flake 使用相同版本的 nixpkgs
     nur.inputs.nixpkgs.follows = "nixpkgs";
+
+    # 远程部署
+    deploy-rs = {
+      url = "github:serokell/deploy-rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   # 引入 nixos-cn flake 作为 inputs
@@ -36,6 +42,7 @@
     home-manager,
     nixos-cn,
     nur,
+    deploy-rs,
     ...
   } @ inputs: {
     nixosConfigurations = {
@@ -115,6 +122,45 @@
           }
         ];
       };
-    };
+
+      #
+      "start-nixos" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # hardware configuration
+          ./hosts/r9000p/hardware-configuration.nix
+          # basic configuration
+          ./hosts/start/start.nix
+        ];
+      };
+    }; #end nixosConfigurations
+
+    # 远程部署
+    deploy = {
+      sshUser = "root"; # SSH 登录用户名
+      user = "root"; # 远程操作的用户
+      sshOpts = ["-p" "22"]; # SSH 参数，这里是指定端口 2222
+
+      # 部署失败自动回滚，建议关闭
+      # 因为 NixOS（尤其是 Unstable 分支）部署不太稳定，有时需要部署两次才成功
+      # 如果自动回滚了，反而适得其反，导致连续部署失败
+      autoRollback = false;
+
+      # 断网自动回滚，建议关闭
+      # 在你配置防火墙或 IP 出错把网络干掉时，自动回滚，这样你就不用去主机商控制面板连 VNC 或 IPMI 了
+      # 但如果你就是在调整防火墙或者 IP 配置，会有当时断网、但重启机器就可以应用新配置恢复正常的情况
+      # 自动回滚反而适得其反，因此建议关闭
+      magicRollback = false;
+      nodes = {
+        "start-nixos" = {
+          # 目标机器的地址，IP 或域名或 .ssh/config 中配置的别名均可
+          hostname = "172.20.10.6";
+          profiles.system = {
+            # 调用上面的 nixosConfigurations."nixos"
+            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations."start-nixos";
+          };
+        };
+      };
+    }; # end deploy-rs
   };
 }
---
+++ hosts/start/start.nix
@@ -0,0 +1,126 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  boot.loader = {
+    grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+      extraEntries = ''
+        menuentry "Windows" {
+            search --file --no-floppy --set=root /EFI/Microsoft/Boot/bootmgfw.efi
+                chainloader (''${root})/EFI/Microsoft/Boot/bootmgfw.efi
+        }
+      '';
+    };
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+  };
+
+  networking = {
+    hostName = "r9000p-nixos";
+    networkmanager.enable = true;
+  };
+
+  # Set your time zone.
+  time.timeZone = "Asia/Shanghai";
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+
+  security.rtkit.enable = true;
+
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    trusted-users = ["npc"];
+    substituters = [
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      "https://cache.nixos.org"
+    ];
+  };
+
+  # Enable the OpenSSH daemon.
+  services = {
+    openssh = {
+      enable = true;
+      permitRootLogin = "yes";
+    };
+  };
+
+  environment.variables.EDITOR = "vim";
+
+  users.users.npc = {
+    isNormalUser = true;
+    description = "npc";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    cmake
+    unzip
+    which
+    tree
+    git
+    vim
+    curl
+    wget
+    # tools
+    foot
+    #
+    neofetch
+    btop
+    lsof
+  ];
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
