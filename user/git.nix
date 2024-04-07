c35915998a00abe88a156f9e2024762e1ef45a69
npc-z <1763998996@qq.com>
2024-04-07 14:02:18 +0800

    fix git lg alias

--- user/git.nix
+++ user/git.nix
@@ -8,9 +8,7 @@
       co = "checkout";
       br = "branch";
       last = "log -1";
-      lg = ''
-        log --color --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit safe.directory
-      '';
+      lg = "log --color --graph --abbrev-commit --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'";
     };
     extraConfig = {
       init = {
