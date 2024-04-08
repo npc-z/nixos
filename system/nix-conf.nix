b001f65cd442f1dcd3162669b91471a0adbe4769
npc-z <1763998996@qq.com>
2024-04-08 21:35:17 +0800

    add hyprland cachix

--- system/nix-conf.nix
+++ system/nix-conf.nix
@@ -22,6 +22,9 @@
 
         "https://cache.nixos.org"
 
+        # for hyprland
+        "https://hyprland.cachix.org"
+
         # nix community's cache server
         "https://nix-community.cachix.org"
       ];
@@ -32,6 +35,9 @@
 
         # nix community's cache server public key
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+
+        # hyprland
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
 
