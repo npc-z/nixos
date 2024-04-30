0b173a2a332e66944cce144aa4ac03d3588890ba
npc-z <1763998996@qq.com>
2024-04-30 20:53:11 +0800

    revert commit c2e793e(修复 neovim 中 mason 不能正常（动态）调用可执行文件问题)

--- system/zsh.nix
+++ system/zsh.nix
@@ -28,7 +28,7 @@ in {
         source ${fzf_zsh}
         source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
 
-        export NIX_LD=$(nix eval --impure --raw --expr 'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; in NIX_LD')
+        # export NIX_LD=$(nix eval --impure --raw --expr 'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; in NIX_LD')
       '';
       shellAliases = {
         "vim" = "nvim";
