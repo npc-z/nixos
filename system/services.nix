ad2f22007abee89f95752d02435873eac1f16b10
npc-z <1763998996@qq.com>
2024-05-02 19:25:17 +0800

    笔记本合盖不要 suspend

--- system/services.nix
+++ system/services.nix
@@ -1,12 +1,49 @@
 {...}: {
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+
   services.v2raya.enable = true;
 
   services.cron.enable = true;
 
   services.gvfs.enable = true;
   services.udisks2.enable = true;
+
+  services.logind = {
+    lidSwitch = "ignore";
+    extraConfig = ''
+      HandlePowerKey=ignore
+    '';
+  };
+
+  services.acpid = {
+    enable = true;
+    # inspired by https://www.reddit.com/r/NixOS/comments/14qa7d8/control_both_suspend_and_brightness_on_lid_close
+
+    # dont work for me
+    lidEventCommands = ''
+      # export PATH=$PATH:/run/current-system/sw/bin
+      export PATH=/run/wrappers/bin:/run/current-system/sw/bin:$PATH
+      export DISPLAY=":0.0"
+
+      export LID_STATE=$(cat /proc/acpi/button/lid/LID/state | awk '{print $NF}')
+      if [[ $LID_STATE == "closed" ]]; then
+        # mute
+        pamixer -m
+        # lock with hyprlock
+        hyprlock
+
+      else
+        # umute
+        pamixer -u
+      fi
+    '';
+
+    # work for me
+    powerEventCommands = ''
+      systemctl suspend
+    '';
+  };
 
   services.keyd = {
     enable = true;
