845fcae741afb2cb15d0648562aa9edff343d905
npc-z <1763998996@qq.com>
2024-05-03 16:08:18 +0800

    [libvirt] add vm support

---
+++ system/virt/libvirt.nix
@@ -0,0 +1,39 @@
+{
+  pkgs,
+  settings,
+  ...
+}: {
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [pkgs.OVMFFull.fd];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+
+  users.users.${settings.user.username}.extraGroups = ["libvirtd"];
+
+  environment.systemPackages = with pkgs; [
+    spice
+    spice-gtk
+    spice-protocol
+    virt-viewer
+    virtio-win
+    win-spice
+  ];
+  programs.virt-manager.enable = true;
+
+  home-manager.users.${settings.user.username} = {
+    dconf.settings = {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = ["qemu:///system"];
+        uris = ["qemu:///system"];
+      };
+    };
+  };
+}
