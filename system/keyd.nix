f5d61157b0fd631cef9af6059e28c5808e3c978e
npc-z <1763998996@qq.com>
2024-05-19 11:14:36 +0800

    [keyd] reconfig
    使用 nix 的方式配置时导致 ctrl 键出现自动误触发，
    改用直接配置 /etc/keyd/default.conf 的方式

---
+++ system/keyd.nix
@@ -0,0 +1,40 @@
+{...}: let
+  keydConfig = builtins.readFile ./../dotfiles/keyd/default.conf;
+in {
+  environment.etc."keyd/default.conf".text = keydConfig;
+
+  services.keyd = {
+    enable = true;
+    # keyboards = {
+    #   default = {
+    #     ids = ["*"];
+    #     settings = {
+    #       main = {
+    #         # capslock = "control"; # do not work
+    #         capslock = "oneshot(control)";
+    #         "'" = ''"'';
+    #       };
+    #       control = {
+    #         h = "left";
+    #         j = "down";
+    #         k = "up";
+    #         l = "right";
+    #         # a = "home";
+    #         # e = "end";
+    #       };
+    #       shift = {
+    #         "'" = "'";
+    #       };
+    #       "control+shift" = {
+    #         # select all
+    #         # "a" = "C-a";
+    #         # del to end of line
+    #         # "k" = "C-k";
+    #         # "h" = "backspace";
+    #         # "d" = "delete";
+    #       };
+    #     };
+    #   };
+    # };
+  };
+}
