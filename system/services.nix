c4fb046ba987e826470fc5dc76d2f0bcc9ac2523
npc-z <1763998996@qq.com>
2024-05-19 11:14:36 +0800

    [keyd] reconfig
    使用 nix 的方式配置时导致 ctrl 键出现自动误触发，
    改用直接配置 /etc/keyd/default.conf 的方式

--- system/services.nix
+++ system/services.nix
@@ -8,39 +8,4 @@
 
   services.gvfs.enable = true;
   services.udisks2.enable = true;
-
-  services.keyd = {
-    enable = true;
-    keyboards = {
-      default = {
-        ids = ["*"];
-        settings = {
-          main = {
-            # capslock = "control"; # do not work
-            capslock = "oneshot(control)";
-            "'" = ''"'';
-          };
-          control = {
-            h = "left";
-            j = "down";
-            k = "up";
-            l = "right";
-            # a = "home";
-            # e = "end";
-          };
-          shift = {
-            "'" = "'";
-          };
-          "control+shift" = {
-            # select all
-            # "a" = "C-a";
-            # del to end of line
-            # "k" = "C-k";
-            # "h" = "backspace";
-            # "d" = "delete";
-          };
-        };
-      };
-    };
-  };
 }
