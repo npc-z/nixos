211627e08dc8902e12d20647eb72a64e57e5cff9
npc-z <1763998996@qq.com>
2024-06-24 18:54:38 +0800

    [vscode] update extension list

--- user/vscode/default.nix
+++ user/vscode/default.nix
@@ -2,13 +2,38 @@
   programs.vscode = {
     enable = true;
     extensions = with pkgs.vscode-extensions; [
-      vscodevim.vim
-      yzhang.markdown-all-in-one
+      # adpyke.vscode-sql-formatter  # install from vscode-extensions market
+      christian-kohler.path-intellisense
+      # codeium.codeium
+      cweijan.dbclient-jdbc
+      # cweijan.vscode-mysql-client2
+      # cweijan.vscode-office
+      # cweijan.xmind-viewer
+      davidanson.vscode-markdownlint
       eamodio.gitlens
+      # emeraldwalk.runonsave
+      esbenp.prettier-vscode
+      formulahendry.code-runner
+      gruntfuggly.todo-tree
+      humao.rest-client
+      mechatroner.rainbow-csv
+      # mongodb.mongodb-vscode
+      ms-python.debugpy
+      ms-python.isort
       ms-python.python
       ms-python.vscode-pylance
+      ms-toolsai.jupyter
+      ms-toolsai.jupyter-keymap
+      ms-toolsai.jupyter-renderers
+      ms-toolsai.vscode-jupyter-cell-tags
+      ms-toolsai.vscode-jupyter-slideshow
+      # njqdev.vscode-python-typehint
       pkief.material-icon-theme
-      pkief.material-icon-theme
+      # semanticdiff.semanticdiff
+      shd101wyy.markdown-preview-enhanced
+      visualstudioexptteam.intellicode-api-usage-examples
+      vscodevim.vim
+      yzhang.markdown-all-in-one
     ];
   };
 }
