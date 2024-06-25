a6cd943f89f6c5cb43892984824d485e9fe7ed8a
npc-z <1763998996@qq.com>
2024-06-25 17:39:12 +0800

    [dotfiles git] display the commit time in "git lg"

--- dotfiles/config-git.sh
+++ dotfiles/config-git.sh
@@ -10,7 +10,8 @@ git config --global alias.st status --replace-all
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.last 'log -1'
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+# git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.lg  "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr [%ad]) %C(bold blue)<%an>%Creset' --abbrev-commit --date=format:'%Y-%m-%d %H:%M:%S'"
 
 # 正确显示中文文件名
 git config --global core.quotepath false
