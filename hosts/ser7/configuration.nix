657dc6c06358f05ffb111788c2e8f298bf978c9a
npc-z <1763998996@qq.com>
2024-10-07 16:32:50 +0800

    [ollama] add ollama and openwebui services

--- hosts/ser7/configuration.nix
+++ hosts/ser7/configuration.nix
@@ -3,7 +3,11 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./../../system/steam.nix
+
+    ./../../system/ollama/ser7.nix
   ];
+
+  nixpkgs.config.rocmSupport = true;
 
   networking = {
     hostName = "ser7-nixos";
