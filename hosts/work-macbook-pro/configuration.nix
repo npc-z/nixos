0fa56684501e6c9285f5202bfb1d37ad7d789eb0
npc-z <1763998996@qq.com>
2025-04-01 13:13:00 +0800

    [darwin] 合并配置文件

--- hosts/work-macbook-pro/configuration.nix
+++ hosts/work-macbook-pro/configuration.nix
@@ -1,6 +1,26 @@
-{mylib, ...}: {
+{
+  mylib,
+  myvars,
+  ...
+}: let
+  username = myvars.username;
+  hostname = "work-macbook-pro";
+in {
   imports = [
     (mylib.relativeToRoot "modules/darwin/configuration.nix")
-    ./users.nix
   ];
+
+  config = {
+    networking.hostName = hostname;
+    networking.computerName = hostname;
+    system.defaults.smb.NetBIOSName = hostname;
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users."${username}" = {
+      home = "/Users/${username}";
+      description = username;
+    };
+
+    nix.settings.trusted-users = [username];
+  };
 }
