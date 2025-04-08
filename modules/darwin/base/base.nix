3480403726e7526f061f0b21ebcb5ad46b2cd3dc
npc-z <1763998996@qq.com>
2025-04-08 15:38:33 +0800

    [darwin nix] 清理 nix 重复配置

--- modules/darwin/base/base.nix
+++ modules/darwin/base/base.nix
@@ -67,36 +67,6 @@
     # description = myvars.username;
   };
 
-  nix.settings = {
-    # enable flakes globally
-    experimental-features = ["nix-command" "flakes"];
-
-    # given the users in this list the right to specify additional substituters via:
-    #    1. `nixConfig.substituers` in `flake.nix`
-    #    2. command line args `--options substituers http://xxx`
-    trusted-users = [
-      myvars.username
-    ];
-
-    # substituers that will be considered before the official ones(https://cache.nixos.org)
-    substituters = [
-      # cache mirror located in China
-      # status: https://mirrors.ustc.edu.cn/status/
-      "https://mirrors.ustc.edu.cn/nix-channels/store"
-      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-      # status: https://mirror.sjtu.edu.cn/
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
-
-      "https://nix-community.cachix.org"
-    ];
-
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-
-    builders-use-substitutes = true;
-  };
-
   # Set Git commit hash for configuration version.
   system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
 }
