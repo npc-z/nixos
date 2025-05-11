4241e186cbd272cd8643b6892627f786b6dc2746
npc <1763998996@qq.com>
2025-05-11 13:42:31 +0800

    [home packages] add lsd/erdtree

--- home/base/packages.nix
+++ home/base/packages.nix
@@ -4,10 +4,13 @@
     nix-search-tv
     # Collection of common network programs
     inetutils
+    lsd # Next gen ls command
+    erdtree # File-tree visualizer and disk usage analyzer
   ];
 
   home.shellAliases = {
     # Fuzzy search for Nix packages
     ns = "nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history";
+    ls = "lsd";
   };
 }
