3a31873ae2adf11a51ed0d7129c750957d29bacd
npc <1763998996@qq.com>
2025-06-20 22:34:54 +0800

    [dotfiles] 改进 waybar 设置（pamixer）音量

---
+++ dotfiles/waybar/.config/waybar/scripts/vol.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+adjust_all_sinks() {
+  local op="$1"
+  local amount="${2:-5}"  # 默认加/减 5
+
+  case "$op" in
+    up)
+      flag="-i"
+      ;;
+    down)
+      flag="-d"
+      ;;
+    mute)
+      pamixer --list-sinks \
+        | tail -n +2 \
+        | awk '{print $2}' \
+        | tr -d '"' \
+        | xargs -I{} pamixer --sink {} --toggle-mute
+      return
+      ;;
+    *)
+      echo "Usage: $0 {up|down|mute} [amount]" >&2
+      exit 1
+      ;;
+  esac
+
+  pamixer --list-sinks \
+    | tail -n +2 \
+    | awk '{print $2}' \
+    | tr -d '"' \
+    | xargs -I{} pamixer --sink {} "$flag" "$amount"
+}
+
+# 执行
+adjust_all_sinks "$@"
+
