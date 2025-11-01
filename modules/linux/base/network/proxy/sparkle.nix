cfe5d0448e4dafdb02b46bdeb295dc8122a0793c
npc-z <1763998996@qq.com>
2025-11-01 20:49:27 +0800

    feat(network): add sparkle(vpn)

---
+++ modules/linux/base/network/proxy/sparkle.nix
@@ -0,0 +1,20 @@
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = [
+    pkgs.sparkle
+  ];
+
+  security.wrappers.sparkle = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_net_bind_service,cap_net_raw,cap_net_admin=+ep";
+    source = lib.getExe pkgs.sparkle;
+  };
+
+  networking.proxy.default = "http://127.0.0.1:20171";
+
+  networking.firewall.trustedInterfaces = ["mihomo"];
+}
