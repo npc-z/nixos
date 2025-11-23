66f5ec21de93d22b0d4a8d0e31dd504d22c579f6
npc-z <1763998996@qq.com>
2025-11-23 20:36:05 +0800

    feat(home): add lazydocker

--- home/base/tools/dev.nix
+++ home/base/tools/dev.nix
@@ -3,6 +3,8 @@
     home.packages = with pkgs; [
       # Modern API client that lives in your terminal
       posting
+      # Simple terminal UI for both docker and docker-compose
+      lazydocker
     ];
   };
 }
