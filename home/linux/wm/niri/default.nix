e3f9b3a9d6ce5111207cac2dc115c46ba61dd2a5
npc-z <1763998996@qq.com>
2025-11-23 20:37:32 +0800

    feat(niri): config xdg portal

--- home/linux/wm/niri/default.nix
+++ home/linux/wm/niri/default.nix
@@ -20,6 +20,15 @@ in {
   config = mkIf cfg.enable {
     # programs.niri.enable = true;
 
+    xdg.portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-gnome
+      ];
+      config.common.default = "*";
+    };
+
     wm.addons = {
       hyprlock.enable = true;
     };
