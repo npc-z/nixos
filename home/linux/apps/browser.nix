ce389611767fbfe1875b6973c59b37eba504a792
npc-z <1763998996@qq.com>
2025-12-30 22:16:51 +0800

    feat(zen-browser): remove extension: wappalyzer

--- home/linux/apps/browser.nix
+++ home/linux/apps/browser.nix
@@ -71,10 +71,6 @@
         Fingerprinting = true;
       };
       ExtensionSettings = {
-        "wappalyzer@crunchlabz.com" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/wappalyzer/latest.xpi";
-          installation_mode = "force_installed";
-        };
         "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/github-file-icons/latest.xpi";
           installation_mode = "force_installed";
