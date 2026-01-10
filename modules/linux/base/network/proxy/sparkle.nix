a0c729af368ae131e426ce969530d28cfd66d9b2
npc-z <1763998996@qq.com>
2026-01-10 10:11:56 +0800

    fix(proxy): update sparkle package source to use nur repository
    https://github.com/INKCR0W/sparkle

--- modules/linux/base/network/proxy/sparkle.nix
+++ modules/linux/base/network/proxy/sparkle.nix
@@ -4,14 +4,14 @@
   ...
 }: {
   environment.systemPackages = [
-    pkgs.sparkle
+    pkgs.nur.repos.lonerOrz.sparkle
   ];
 
   security.wrappers.sparkle = {
     owner = "root";
     group = "root";
     capabilities = "cap_net_bind_service,cap_net_raw,cap_net_admin=+ep";
-    source = lib.getExe pkgs.sparkle;
+    source = lib.getExe pkgs.nur.repos.lonerOrz.sparkle;
   };
 
   networking.proxy.default = "http://127.0.0.1:20171";
