eacecdceecdb3ad4b8581ea440c8a21929f7e976
npc-z <1763998996@qq.com>
2026-07-05 14:37:17 +0800

    feat(bluetooth): enable Experimental feature

--- modules/linux/base/bluetooth.nix
+++ modules/linux/base/bluetooth.nix
@@ -2,6 +2,12 @@
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
   services.blueman.enable = true;
+  hardware.bluetooth.settings = {
+    General = {
+      # 开启 BlueZ 实验性功能（解决“不显示电量”的关键）
+      Experimental = true;
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     blueman
