a09d72866bc35a9f59a4d737af93a8e9a9457936
npc-z <1763998996@qq.com>
2026-07-30 11:08:57 +0800

    fix(hpyrland): update spring animation to fit v0.56 and correct gestures

--- dotfiles/hypr/.config/hypr/animations.lua
+++ dotfiles/hypr/.config/hypr/animations.lua
@@ -11,11 +11,11 @@ hl.curve("menu_decel", { type = "bezier", points = { { 0.1, 1 }, { 0, 1 } } })
 hl.curve("menu_accel", { type = "bezier", points = { { 0.38, 0.04 }, { 1, 0.07 } } })
 
 -- Spring Curves
-hl.curve("spring_menu", { type = "spring", mass = 1, stiffness = 80, dampening = 14 })
-hl.curve("spring_window", { type = "spring", mass = 1, stiffness = 30, dampening = 8 })
-hl.curve("spring_open", { type = "spring", mass = 1, stiffness = 30, dampening = 8 })
-hl.curve("spring_workspace", { type = "spring", mass = 1.2, stiffness = 30, dampening = 10 })
-hl.curve("spring_special", { type = "spring", mass = 1, stiffness = 30, dampening = 8 })
+hl.curve("spring_menu", { type = "spring", mass = 1, stiffness = 870, dampening = 55 })
+hl.curve("spring_window", { type = "spring", mass = 1, stiffness = 870, dampening = 55 })
+hl.curve("spring_open", { type = "spring", mass = 1, stiffness = 870, dampening = 55 })
+hl.curve("spring_workspace", { type = "spring", mass = 1, stiffness = 870, dampening = 55 })
+hl.curve("spring_special", { type = "spring", mass = 1, stiffness = 870, dampening = 55 })
 
 hl.animation({ leaf = "workspaces", enabled = true, speed = 5, bezier = "easeOutExpo", style = "slidevert" })
 hl.animation({ leaf = "specialWorkspaceIn", enabled = true, speed = 3, bezier = "easeOutExpo", style = "slidevert" })
--- dotfiles/hypr/.config/hypr/gesture.lua
+++ dotfiles/hypr/.config/hypr/gesture.lua
@@ -1,6 +1,6 @@
 hl.config({
     gestures = {
-        workspace_swipe_distance = 200,
+        workspace_swipe_distance = 100,
 
         -- whether a swipe right on the last workspace should create a new one
         workspace_swipe_create_new = false,
@@ -11,38 +11,4 @@ hl.gesture({ fingers = 2, direction = "pinch", action = "cursorZoom", zoom_level
 
 hl.gesture({ fingers = 3, direction = "vertical", action = "workspace" })
 
--- FIXME: refract
-hl.gesture({
-    fingers = 3,
-    direction = "left",
-    ---@diagnostic disable-next-line: assign-type-mismatch
-    action = function()
-        hl.notification.create({ text = "I just swiped on my trackpad LEFT!", duration = 3000, icon = "ok" })
-        hl.dsp.layout("move -col")
-        -- hl.dispatch(hl.dsp.window.cycle_next()) -- Change focus to another window
-        -- hl.dispatch(hl.dsp.window.bring_to_top()) -- Bring it to the top
-    end,
-})
-
-hl.gesture({
-    fingers = 3,
-    direction = "right",
-    ---@diagnostic disable-next-line: assign-type-mismatch
-    action = function()
-        hl.notification.create({ text = "I just swiped on my trackpad RIGHT!", duration = 3000, icon = "ok" })
-        hl.dsp.layout("focus r")
-        -- hl.dsp.focus({ direction = "right" })
-        -- hl.dsp.layout("swapcol l")
-        -- hl.dispatch(hl.dsp.window.cycle_next()) -- Change focus to another window
-        -- hl.dispatch(hl.dsp.window.bring_to_top()) -- Bring it to the top
-    end,
-})
-
-hl.gesture({
-    fingers = 4,
-    direction = "down",
-    mods = "SUPER",
-    action = "special",
-    workspace_name = "scratchpad",
-    disable_inhibit = true,
-})
+hl.gesture({ fingers = 3, direction = "horizontal", action = "scroll_move" })
