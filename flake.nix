aa0be5b681129487c1b5c44cd7a619c0df8a94b8
npc-z <1763998996@qq.com>
2026-08-05 14:02:44 +0800

    refactor(base): move China cache mirrors to nix-core config

--- flake.nix
+++ flake.nix
@@ -4,31 +4,16 @@
   # the nixConfig here only affects the flake itself, not the system configuration!
   nixConfig = {
     abort-on-warn = true;
-    # Query the mirror of USTC first, and then the official cache.
-    substituters = [
-      # cache mirror located in China
-      "https://mirrors.ustc.edu.cn/nix-channels/store"
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
-      # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-      "https://cache.nixos.org"
-    ];
-
     # extra-substituters will be appended to the default substituters when fetching packages
     extra-substituters = [
       # llm-agents
       "https://cache.numtide.com" # Nix packages for AI coding agents and development tools
-      # for hyprland
-      "https://hyprland.cachix.org"
-
       # noctalia
       "https://noctalia.cachix.org"
     ];
     extra-trusted-public-keys = [
       # llm-agents
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" # Nix packages for AI coding agents and development tools
-      # hyprland
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-
       # noctalia
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
--- modules/base/nix-core.nix
+++ modules/base/nix-core.nix
@@ -32,12 +32,9 @@
       # substituers that will be considered before the official ones(https://cache.nixos.org)
       substituters = [
         # cache mirror located in China
-        # "https://mirrors.ustc.edu.cn/nix-channels/store"
-        # "https://mirror.sjtu.edu.cn/nix-channels/store"
+        "https://mirrors.ustc.edu.cn/nix-channels/store"
+        "https://mirror.sjtu.edu.cn/nix-channels/store"
         # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-
-        "https://cache.nixos.org"
-
         # nix community's cache server
         "https://nix-community.cachix.org"
       ];
@@ -45,7 +42,6 @@
       trusted-public-keys = [
         # the default public key of cache.nixos.org, it's built-in, no need to add it here
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-
         # nix community's cache server public key
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
